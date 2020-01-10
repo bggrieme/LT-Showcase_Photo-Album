6d2e379826273aeffe97800107cee0402f36eeb3
Benjamin Grieme <bgrieme@gmail.com>
2020-01-10 13:53:50 -0600

    Initial commit.

---
+++ src/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net; //WebClient class
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace LT_Showcase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //initial research process..
+            WebClient wc = new WebClient();
+            string json = wc.DownloadString("https://jsonplaceholder.typicode.com/photos");
+            JArray rss = JArray.Parse(json);
+
+            var photos =
+                from data in rss
+                select new
+                {
+                    albumId = (string)data["albumId"],
+                    id = (string)data["id"],
+                    title = (string)data["title"],
+                    url = (string)data["url"],
+                    thumbnailUrl = (string)data["thumbnailUrl"]
+                };
+
+            var queryPhotos =
+                from all in photos
+                where all.albumId == "3"
+                select all;
+
+
+
+            Console.WriteLine(rss[0].ToString());
+
+
+
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
