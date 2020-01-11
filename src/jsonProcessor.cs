816d3372e20a9c26100dd4f15c52ae6cf00e28ff
bgrieme <bgrieme@gmail.com>
2020-01-11 01:01:24 -0600

    Initial commit. Implemented constructor, added method skeleton for id_and_title_by_album query. Included some TODO comments.

--- src/jsonProcessor.cs
+++ src/jsonProcessor.cs
@@ -7,12 +7,19 @@ namespace LT_Showcase
     public class jsonProcessor
     {
         JArray token_array {get; private set;}
-        public jsonProcessor(string jsonAddress)
+        public jsonProcessor(string jsonWebAddress)
         {
             System.Net.WebClient wc = new System.Net.WebClient();
-            this.token_array = JArray.Parse(wc.DownloadString(jsonAddress)); //WebClient downloads the given address as a string, and JArray.Parse breaks that string into individual json tokens which are then stored in token_array
+            this.token_array = JArray.Parse(wc.DownloadString(jsonWebAddress)); //WebClient downloads the given address as a string, and JArray.Parse breaks that string into individual json tokens which are then stored in token_array
         }
+        
 
-        //TODO: write stubs. consider dynamic return type for query methods. Write some tests.
+        //TODO: write stubs. Write some tests. Implement stubs. Run tests, etc.
+        
+        //basic query function
+        public string id_and_title_by_album(){
+            //TODO: design thoughts - probably best to pass a specific album id as an argument and build the return based off of that
+            //as opposed to processing the entire json at once. A little more future-proof this way.
+        }
     }
 }
