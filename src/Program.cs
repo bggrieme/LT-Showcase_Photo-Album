a15f14f28448d234f7d28ca8f3ddf52711dbb420
Benjamin Grieme <bgrieme@gmail.com>
2020-01-28 00:36:06 -0600

    Now clears console between loops. Adjusted loop logic to work with new PhotoProcessor changes.

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;//Console
+﻿using System;
 using System.Collections.Generic;
 
 namespace LTShowcase
@@ -10,13 +10,14 @@ namespace LTShowcase
             string address = "https://jsonplaceholder.typicode.com/photos";
             InputManager inputManager = new InputManager();
             PhotoProcessor photoProcessor = new PhotoProcessor(address);
-
             bool willContinue = true;
             while (willContinue)
             {
+                Console.Clear();
                 Console.WriteLine("Please enter which albums you would like to view. e.g. \"1-5, 8, 7-10\"");
                 List<int> albumIDs = inputManager.ParseInputToList(Console.ReadLine());
-                Console.WriteLine(photoProcessor.PhotoID_andTitle_GroupedBy_Album(albumIDs));
+                photoProcessor.DownloadPhotos(albumIDs);
+                Console.WriteLine(photoProcessor.PhotoIDsAndTitlesGroupedByAlbum());
                 Console.WriteLine("Would you like to view another set of albums? y / n");
                 string continueResponse = Console.ReadLine();
                 if (continueResponse == "n")
