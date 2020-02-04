75f158cefe012c7c99b69c9551d68e4424f0999c
Benjamin Grieme <bgrieme@gmail.com>
2020-02-03 18:15:33 -0600

    Reconfigured to use new design.

--- src/Program.cs
+++ src/Program.cs
@@ -7,17 +7,15 @@ namespace LTShowcase
     {
         static void Main(string[] args)
         {
-            string address = "https://jsonplaceholder.typicode.com/photos";
-            InputManager inputManager = new InputManager();
-            PhotoProcessor photoProcessor = new PhotoProcessor(address);
+            string SourceAddress = "https://jsonplaceholder.typicode.com/photos";
+            JsonQueryManager QueryManager = new JsonQueryManager(SourceAddress);
             bool willContinue = true;
             while (willContinue)
             {
                 Console.Clear();
                 Console.WriteLine("Please enter which albums you would like to view. e.g. \"1-5, 8, 7-10\"");
-                List<int> albumIDs = inputManager.ParseInputToList(Console.ReadLine());
-                photoProcessor.DownloadPhotos(albumIDs);
-                Console.WriteLine(photoProcessor.PhotoIDsAndTitlesGroupedByAlbum());
+                string input = Console.ReadLine();
+                Console.WriteLine(QueryManager.GetQueriedAlbumsString(input));
                 Console.WriteLine("Would you like to view another set of albums? y / n");
                 string continueResponse = Console.ReadLine();
                 if (continueResponse == "n")
